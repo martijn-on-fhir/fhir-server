a037bf4465d6140f52cc32d3650af8f39f905c09
AzureAD\MartijnSchimmel <m.schimmel@adapcare.nl>
2025-07-23 16:06:50 +0200

    Standardize formatting and add semicolons across `ValidationService`:
    - Improved code consistency by ensuring semicolon usage throughout the service.
    - Standardized spacing and removed redundant comments.
    - Updated method signatures to align with TypeScript best practices.

--- src/services/validation/validation.service.ts
+++ src/services/validation/validation.service.ts
@@ -124,7 +124,7 @@ export class ValidationService {
         //   }
         // }
         
-        this.validateElement(resource, elementPath, elementDefinition, errors, warnings);
+        this.validateElement(resource, elementPath, elementDefinition, errors)
       }
       
       return {
