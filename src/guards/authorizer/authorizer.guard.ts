8e96b73decdd529219d664a91235e4774da70914
AzureAD\MartijnSchimmel <m.schimmel@adapcare.nl>
2025-07-30 19:28:13 +0200

    Enable OAuth and integrate `AuthorizerGuard` in `FhirController` for enhanced basic security.

---
+++ src/guards/authorizer/authorizer.guard.ts
@@ -0,0 +1,33 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { ConfigService } from '@nestjs/config';
+import * as jose from 'jose'
+import { JWTPayload } from 'jose';
+
+@Injectable()
+export class AuthorizerGuard implements CanActivate {
+  
+  constructor(private readonly _config: ConfigService) {
+  }
+  
+  canActivate( context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+    
+    if(this._config.get('authorization.enabled') === true){
+      
+      const request = context.switchToHttp().getRequest()
+      const authHeader = request.headers['authorization']
+      
+      if (authHeader) {
+        
+        const decodeJwt: any = jose.decodeJwt(authHeader)
+        
+        return decodeJwt ? decodeJwt.exp > Date.now() / 1000 : false
+        
+      } else {
+        return false
+      }
+    }
+    
+    return true;
+  }
+}
